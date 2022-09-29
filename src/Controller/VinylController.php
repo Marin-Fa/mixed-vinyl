3c4792902243b5c926f2e8e13ada324bbc7ba93a
Marin-Fa <marin.fayolle@gmail.com>
2022-09-29 10:25:51 +0200

    Added some Twiggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,25 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use symfony\component\string\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        // A controller should return a Response object of HTTPfoundation
-        return new Response('Title: PB and Jams');
-    }
+        // A controller should always return a Response object of HTTPfoundation
+        //return new Response('Title: PB and Jams');
+        // La méthode render retourne une response
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig',
+            [
+                'title' => 'PB and Jams',
+                'tracks' => $tracks,
 
-    //#[Route('/browse/{slug}')]
-//    public function browse(string $slug): Response
-//    {
-//        $title = u(str_replace('-', ' ', $slug))->title(true);
-//        return new Response('Genre: ' . $title);
-//
-//    }
+            ]
+        );
+    }
 
 }
