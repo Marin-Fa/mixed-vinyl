d7f62c0c1085ad37ea7d3eba9528b1fd28680b0f
Marin-Fa <marin.fayolle@gmail.com>
2022-09-29 16:24:32 +0200

    song controller, loggerInterface

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id): Response
+    // the route should only take a wildcard number
+    // that would gave a 500 error server
+    // that'll gave a 404 error that's better
+    #[Route('/api/songs/{id</d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info('Returning API Response for song {song}' . $id, [
+            'song' => $id,
+        ]);
         // json encode
         return new JsonResponse($song);
     }
--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(): Response
+    public function homepage(Environment $twig): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -21,10 +22,12 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
-        return $this->render('vinyl/homepage.html.twig', [
-            'title' => 'PB & Jams',
+        $html = $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Marin & Jams',
             'tracks' => $tracks,
         ]);
+
+        return new Response($html);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
