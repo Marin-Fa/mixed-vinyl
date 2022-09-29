062176f2e482d14a8b0d3c5a424322252c327b7a
Marin-Fa <marin.fayolle@gmail.com>
2022-09-29 15:40:05 +0200

    Smart Routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,16 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use symfony\component\string\u;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
-        // A controller should always return a Response object of HTTPfoundation
-        //return new Response('Title: PB and Jams');
-        // La méthode render retourne une response
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
             ['song' => 'Waterfalls', 'artist' => 'TLC'],
@@ -23,13 +20,20 @@ class VinylController extends AbstractController
             ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
-        return $this->render('vinyl/homepage.html.twig',
-            [
-                'title' => 'PB and Jams',
-                'tracks' => $tracks,
 
-            ]
-        );
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(string $slug = null): Response
+    {
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
+    }
 }
