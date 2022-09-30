f68435ee07006cd08238c27e3856d228e1b9cf50
Marin-Fa <marin.fayolle@gmail.com>
2022-09-30 11:46:32 +0200

    Adding knplabs/knp-time-bundle

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -4,28 +4,26 @@ namespace App\Controller;
 
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SongController extends AbstractController
 {
-    // the route should only take a wildcard number
-    // that would gave a 500 error server
-    // that'll gave a 404 error that's better
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'], name: 'api_songs_get_one')]
     public function getSong(int $id, LoggerInterface $logger): Response
     {
+        // TODO query the database
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
 
-        $logger->info('Returning API Response for song {song}' . $id, [
+        $logger->info('Returning API response for song {song}', [
             'song' => $id,
         ]);
-        // json encode
-        return new JsonResponse($song);
+
+        return $this->json($song);
     }
 }
