e228a4ffec022384a48866caa1d72eb9851cb385
Marin-Fa <marin.fayolle@gmail.com>
2022-10-05 13:21:57 +0200

    Configuring dev environment

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\TimeBundle\DateTimeFormatter;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
